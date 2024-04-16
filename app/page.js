1b6364e01973d485a6d907c46fe4d8ecb43f40fe
oussama007 <ohamdi776@gmail.com>
2024-04-16 23:23:42 +0100

    Update Home page

--- app/page.js
+++ app/page.js
@@ -1,35 +1,14 @@
-"use client";
-import styled from "styled-components";
+import PokemonContainer from "./components/PokemonContainer";
+import { getPokemonList, getPokemonTypesList } from "./lib/pokemonApi";
 
-// Define styled button component
-const StyledBtn = styled.button`
-  background-color: ${(props) => (props.red ? "red" : "blue")};
-  color: #fff;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  border: none;
-  font-size: 16px;
-  font-weight: 600;
-  margin-top: 20px;
-  transition: 0.3s;
-  &:hover {
-    transform: scale(1.1);
-  }
-`;
+export default async function Home() {
+  const pokemonList = await getPokemonList();
+  const types = await getPokemonTypesList();
 
-const FancyBtn = styled(StyledBtn)`
-  background-image: linear-gradient(to right, #f6d365 0%, #fda085 100%);
-`;
-
-// Define the Home component
-export default function Home() {
   return (
-    <main>
-      <h1>Pokemon</h1>
-      <StyledBtn red={false}>Click me</StyledBtn>
-      <br />
-      <FancyBtn>Click me</FancyBtn>
-    </main>
+    <PokemonContainer
+      getPokemonList={pokemonList}
+      getPokemonTypesList={types}
+    />
   );
 }
